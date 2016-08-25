5faa156ef6ac754bb43bef6fa0390ec49da83bd4
BBT <zhangtao_43@163.com>
2016-08-25 23:34:43 +0800

    2st update

--- app.js
+++ app.js
@@ -58,15 +58,14 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+if (app.get('env') !== 'development') {
+} else app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
-      message: err.message,
-      error: err
+        message: err.message,
+        error: err
     });
-  });
-}
+});
 
 // production error handler
 // no stacktraces leaked to user
